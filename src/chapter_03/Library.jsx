5ad54605c1cc341f3466701cf034d9060e5e6fdd
wlgggy <kingzihyo@gmail.com>
2024-11-29 13:56:00 +0900

    chapter03/Library

--- src/chapter_03/Library.jsx
+++ src/chapter_03/Library.jsx
@@ -5,7 +5,7 @@ function Library(props){
     return (
         <div>
             <Book name="처음 만난 파이썬" numOfPage={300} />
-            <Book name="처음 만난 AWS" numOfPage={400} />
+            <Book name="처음 만난 JAVA" numOfPage={400} />
             <Book name="처음 만난 리액트" numOfPage={500} />
         </div>
     );
